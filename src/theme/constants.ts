be701689ace43f3e44c299c8db57c2876ad45e39
Onyekachi-Chidiebere <onyekachisamuel18@gmail.com>
2026-06-01 21:17:05 +0200

    added git workflow

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -1,4 +1,5 @@
 
-export const STRIPE_PUBLIC_KEY = 'pk_test_51Qv032FdNOUXRT289ea7PQRfqpSsWt7hyh8XrlUfh1dWAozpzxqJZdyr10fpYPgoJ72sjyGA0NQ79GRpqCfYYzZz00KHWHhrAQ'
-export const BACKEND_URL = 'http:/192.168.8.72:3010';
-// export const BACKEND_URL = 'https://api.dinewell.ca';
+export const STRIPE_PUBLIC_KEY = 'pk_live_51SMahYFDzsWIhDmDrDKwpSWy8kZdt1yX6l6kzm6vs0dxpXDHzA5PL5BTaLXMsK9tSKKBnxq6ETXy6nhldU0VDZ8000X8Xwlu0a'
+// export const STRIPE_PUBLIC_KEY = 'pk_test_51Qv032FdNOUXRT289ea7PQRfqpSsWt7hyh8XrlUfh1dWAozpzxqJZdyr10fpYPgoJ72sjyGA0NQ79GRpqCfYYzZz00KHWHhrAQ'
+// export const BACKEND_URL = 'http:/192.168.8.72:3010';
+export const BACKEND_URL = 'https://api.dinewell.ca';
